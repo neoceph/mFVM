33c88250810234747c31fc65f522d25a28b90efe
Abdullah Al Amin <nirjhor18@gmail.com>
2023-01-15 14:36:02 -0600

    Example writing scalar, vector, tensor completed on write.cpp

--- src/sources/write.cpp
+++ src/sources/write.cpp
@@ -1,83 +1,117 @@
-#include <vtkActor.h>
-#include <vtkCellArray.h>
-#include <vtkDataSetMapper.h>
-#include <vtkNamedColors.h>
+// #include <vtkActor.h>
+// #include <vtkCamera.h>
+// #include <vtkDataSetMapper.h>
+// #include <vtkNamedColors.h>
+// #include <vtkRenderWindow.h>
+// #include <vtkRenderWindowInteractor.h>
+// #include <vtkRenderer.h>
+// #include <vtkLookupTable.h>
+// #include <vtkCellIterator.h>
+#include <vtkCellData.h>
+#include <vtkDoubleArray.h>
 #include <vtkNew.h>
 #include <vtkPointData.h>
-#include <vtkProperty.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkRenderer.h>
-#include <vtkTetra.h>
-#include <vtkUnstructuredGrid.h>
-#include <vtkVertexGlyphFilter.h>
-#include <vtkXMLUnstructuredGridReader.h>
-#include <vtkXMLUnstructuredGridWriter.h>
+#include <vtkStructuredGrid.h>
+#include <vtkXMLStructuredGridWriter.h>
 
-int main(int argc, char* argv[])
+#include <iterator>
+#include <map>
+#include <set>
+
+int main(int, char*[])
 {
-  vtkNew<vtkNamedColors> colors;
 
-  // Parse command line arguments
-  if (argc != 2)
+  size_t nx = 2, ny = 3, nz = 2;
+  auto dataSize = nx * ny * nz;
+
+
+  vtkNew<vtkDoubleArray> scalarValue;
+  scalarValue->SetName("Mass");
+  
+  scalarValue->SetNumberOfComponents(1);
+  scalarValue->SetNumberOfTuples(dataSize);
+  for (size_t i = 0; i < dataSize; ++i)
   {
-    std::cout << "Required arguments: OutputFilename.vtu" << std::endl;
-    return EXIT_FAILURE;
+    scalarValue->SetValue(i, i);
   }
 
-  std::string filename = argv[1];
+  vtkNew<vtkDoubleArray> vectorValue;
+  vectorValue->SetName("Velocity");
+  vectorValue->SetNumberOfComponents(3);
+  vectorValue->SetNumberOfTuples(dataSize);
+  
+  vtkNew<vtkDoubleArray> nodeTensorValues;
+  nodeTensorValues->SetName("stress");
+  nodeTensorValues->SetNumberOfComponents(9);
+  nodeTensorValues->SetNumberOfTuples(dataSize);
+  
+  for (size_t i = 0; i < dataSize; ++i)
+  {
+    // vectorValue->SetValue(i, i);
+    vectorValue->SetTuple3(i, i, 2*i, 3*i);
+    nodeTensorValues->SetTuple9(i, i, 2*i, 3*i, i, 2*i, 3*i, i, 2*i, 3*i);
+  }
 
+  auto numberOfCells = (nx - 1) * (ny - 1) * (nz - 1);
+  vtkNew<vtkDoubleArray> cellScalars;
+  cellScalars->SetName("Density");
+  cellScalars->SetNumberOfTuples(numberOfCells);
+  
+  vtkNew<vtkDoubleArray> cellVectors;
+  cellVectors->SetNumberOfComponents(3);
+  cellVectors->SetName("Acceleration");
+  cellVectors->SetNumberOfTuples(numberOfCells);
+
+  vtkNew<vtkDoubleArray> cellTensorValues;
+  cellTensorValues->SetNumberOfComponents(9);
+  cellTensorValues->SetName("Stress");
+  cellTensorValues->SetNumberOfTuples(numberOfCells);
+
+  for (size_t i = 0; i < numberOfCells; ++i)
+  {
+    cellScalars->SetValue(i, i);
+    cellVectors->SetTuple3(i, i, 2*i, 3*i);
+    cellTensorValues->SetTuple9(i, i, 2*i, 3*i, i, 2*i, 3*i, i, 2*i, 3*i);
+  }
+
+//creating coordinates for the points to be written in structured grid
   vtkNew<vtkPoints> points;
-  points->InsertNextPoint(0, 0, 0);
-  points->InsertNextPoint(1, 0, 0);
-  points->InsertNextPoint(1, 1, 0);
-  points->InsertNextPoint(0, 1, 1);
+  auto x = 0.0;
+  auto y = 0.0;
+  auto z = 0.0;
+  for (unsigned int k = 0; k < nz; k++)
+  {
+    z += 2.0;
+    for (unsigned int j = 0; j < ny; j++)
+    {
+      y += 1.0;
+      for (unsigned int i = 0; i < nx; i++)
+      {
+        x += .5;
+        points->InsertNextPoint(x, y, z); // point values are ascending x-first, y-second, and z-last
+      }
+    }
+  }
 
-  vtkNew<vtkTetra> tetra;
+  // Create a grid
+  vtkNew<vtkStructuredGrid> structuredGrid;
+  // Specify the dimensions of the grid
+  structuredGrid->SetDimensions(static_cast<int>(nx), static_cast<int>(ny),
+                                static_cast<int>(nz));
+  structuredGrid->SetPoints(points);
+  structuredGrid->GetCellData()->AddArray(cellScalars);  //setting scalar values to cell centers
+  structuredGrid->GetCellData()->AddArray(cellVectors);  //setting vector values to cell centers
+  structuredGrid->GetCellData()->AddArray(cellTensorValues);  //setting tensor values to cell centers
+  structuredGrid->GetPointData()->AddArray(scalarValue); //setting scalar values to the cell corners/points
+  structuredGrid->GetPointData()->AddArray(vectorValue); //setting vector values to the cell corners/points
+  structuredGrid->GetPointData()->AddArray(nodeTensorValues); //setting tensor values to the cell corners/points
 
-  tetra->GetPointIds()->SetId(0, 0);
-  tetra->GetPointIds()->SetId(1, 1);
-  tetra->GetPointIds()->SetId(2, 2);
-  tetra->GetPointIds()->SetId(3, 3);
-
-  vtkNew<vtkCellArray> cellArray;
-  cellArray->InsertNextCell(tetra);
-
-  vtkNew<vtkUnstructuredGrid> unstructuredGrid;
-  unstructuredGrid->SetPoints(points);
-  unstructuredGrid->SetCells(VTK_TETRA, cellArray);
 
   // Write file
-  vtkNew<vtkXMLUnstructuredGridWriter> writer;
-  writer->SetFileName(filename.c_str());
-  writer->SetInputData(unstructuredGrid);
+  vtkNew<vtkXMLStructuredGridWriter> writer;
+  writer->SetFileName("output.vts");
+  writer->SetInputData(structuredGrid);
   writer->Write();
-
-  // Read and display file for verification that it was written correclty
-  vtkNew<vtkXMLUnstructuredGridReader> reader;
-  reader->SetFileName(filename.c_str());
-  reader->Update();
-
-  vtkNew<vtkDataSetMapper> mapper;
-  mapper->SetInputConnection(reader->GetOutputPort());
-
-  vtkNew<vtkActor> actor;
-  actor->SetMapper(mapper);
-  actor->GetProperty()->SetColor(colors->GetColor3d("MistyRose").GetData());
-
-  vtkNew<vtkRenderer> renderer;
-  vtkNew<vtkRenderWindow> renderWindow;
-  renderWindow->AddRenderer(renderer);
-  renderWindow->SetWindowName("WriteVTU");
-
-  vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
-  renderWindowInteractor->SetRenderWindow(renderWindow);
-
-  renderer->AddActor(actor);
-  renderer->SetBackground(colors->GetColor3d("cobalt_green").GetData());
-
-  renderWindow->Render();
-  renderWindowInteractor->Start();
 
   return EXIT_SUCCESS;
 }
